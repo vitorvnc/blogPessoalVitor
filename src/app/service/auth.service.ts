e13949064a13952a959022179f377c97b04c198a
vitorvnc <vitorvnc.f@gmail.com>
2021-11-05 21:36:31 -0300

    Component da pagina de Inicio

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -4,6 +4,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { User } from '../model/User';
 import { CredenciaisDTO } from '../model/CredenciaisDTO';
+import { environment } from 'src/environments/environment.prod';
 
 @Injectable({
   providedIn: 'root'
@@ -14,14 +15,24 @@ export class AuthService {
   constructor(private http: HttpClient) { }
 
   entrar(credenciais: CredenciaisDTO): Observable<CredenciaisDTO>{
-    return this.http.put<CredenciaisDTO>('https://vitorvncblogpessoal.herokuapp.com/api/v1/usuarios/credenciais', credenciais)
+    return this.http.put<CredenciaisDTO>('https://vitorvncblogpessoal.herokuapp.com/usuarios/credenciais', credenciais)
   }
 
   entrarDTO(usuarioLoginDTO: UserLoginDTO): Observable<UserLoginDTO>{
-    return this.http.put<UserLoginDTO>('https://vitorvncblogpessoal.herokuapp.com/api/v1/usuarios/credenciais', usuarioLoginDTO)
+    return this.http.put<UserLoginDTO>('https://vitorvncblogpessoal.herokuapp.com/usuarios/credenciais', usuarioLoginDTO)
   }
 
   cadastrar(user: User): Observable<User>{
     return this.http.post<User>('https://vitorvncblogpessoal.herokuapp.com/usuarios/salvar', user)
+  }
+
+  logado(){
+    let ok: boolean = false
+    
+    if (environment.token != ''){
+      ok = true
+    }
+
+    return ok
   }
 }
